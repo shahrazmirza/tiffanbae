729230652af729d5a1a17be21486f754a6f2ca7d
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-02 12:53:20 +1100

    Commit

--- src/app/auth/activation/[jwt]/page.tsx
+++ src/app/auth/activation/[jwt]/page.tsx
@@ -10,7 +10,7 @@ interface Props {
 const ActivationPage = async ({ params }: Props) => {
   const result = await activateUser(params.jwt);
   return (
-    <div className="h-screen flex flex-col items-center justify-center p-5">
+    <div className="h-screen flex flex-col items-center justify-center text-center p-5">
       {result === "userNotExist" ? (
         <p className="text-red-500 text-2xl">Your account does not exist</p>
       ) : result === "alreadyActivated" ? (
