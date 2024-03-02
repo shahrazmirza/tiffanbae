69b9c76eb6847d0bf7ba51237143c1ba2b5c4523
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-02 13:15:31 +1100

    Commit

--- src/app/auth/activation/[jwt]/page.tsx
+++ src/app/auth/activation/[jwt]/page.tsx
@@ -15,19 +15,19 @@ const ActivationPage = async ({ params }: Props) => {
       {result === "userNotExist" ? (
         <div>
           <p className="text-red-500 text-xl md: text-2xl">Your account does not exist</p>
-          <Link href='/auth/signup' className='underline text-blue-600'>Sign up to create an account at Tiffan Bae</Link>
+          <Link href='/auth/signup' className='underline text-white'>Sign up to create an account at Tiffan Bae</Link>
         </div>
       ) : result === "alreadyActivated" ? (
         <div>
           <p className="text-red-500 text-xl md: text-2xl">Your account is already activated</p>
-          <Link href='/auth/signin' className='underline text-blue-600'>Sign in to Tiffan Bae</Link>
+          <Link href='/auth/signin' className='underline text-white'>Sign in to Tiffan Bae</Link>
         </div>
       ) : result === "success" ? (
         <div>
           <p className="text-green-500 text-xl md: text-2xl">
             Congratulations! Your account is now activated
           </p>
-          <Link href='/auth/signin' className='underline text-blue-600'>Sign in to Tiffan Bae</Link>
+          <Link href='/auth/signin' className='underline text-white'>Sign in to Tiffan Bae</Link>
         </div>
       ) : (
         <p className="text-yellow-500 text-xl md: text-2xl">Oops! Something went wrong!</p>
