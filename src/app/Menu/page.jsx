e073642db63912951fa98f3ef5789733a128a0f4
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-05 16:40:29 +1100

    Commit

--- src/app/Menu/page.jsx
+++ src/app/Menu/page.jsx
@@ -41,7 +41,7 @@ const Menu = () => {
       </div>
       
       <div className="w-screen bg-gray-200 py-1">
-        <h2 className='flex flex-col md:flex-row md:text-base text-sm font-semibold justify-center text-center'>
+        <h2 className='flex flex-col md:flex-row md:text-base text-sm font-semibold justify-center text-center md:animate-marquee'>
           Place your order by 12:00 pm for pickup next day <span className='text-red-500 px-2'>OR</span> <p>Place your order by 12:00 pm this Thursday for delivery on Saturday.</p>
         </h2>
       </div>
