84721cb0ac8b065cc4efadb2bb9f3e90e3ab0bdb
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-15 12:36:23 +1100

    Commit

--- src/app/Components/Alert.jsx
+++ src/app/Components/Alert.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes'
 
 const AlertOverlay = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-10  shadow-md flex flex-col item-center">
+      <div className="bg-white p-10 shadow-md flex flex-col items-center">
         <p className="text-base/relaxed font-medium drop-shadow-sm mb-6">{message}</p>
-        <button onClick={onClose} className="flex items-center px-6 text-sm font-medium leading-none border-gray-700 border-solid border rounded text-white hover:text-gray-700 h-10 hover:bg-white bg-gray-700">
+        <Button onClick={onClose} className="px-6 text-sm font-medium leading-none border-gray-700 border-solid border rounded text-white hover:text-gray-700 h-10 hover:bg-white bg-gray-700">
           Close
-        </button>
+        </Button>
       </div>
     </div>
   );
