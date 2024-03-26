efba439d88612335fcc66e111f7363de36fde43e
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-26 16:47:05 +1100

    Commit

--- src/utils/authOptions.tsx
+++ src/utils/authOptions.tsx
@@ -22,6 +22,15 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          image: profile.picture,
+          role: profile.role ? profile.role : "user",
+        };
+      },
       idToken: true,
 
       authorization: {
